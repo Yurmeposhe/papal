13daf06f1e573b9e1b41184152c85162e8c15b24
Yurmeposhe <yurmeposhe@gmail.com>
2023-03-03 20:34:25 -0600

    app

---
+++ app.java
@@ -0,0 +1,6 @@
+We support this is great
+java applications
+.net applications 
+NodeJS applications 
+pyton applications
+web applications
---
+++ userMgt.java
@@ -0,0 +1,7 @@
+landmark
+simon
+dominion
+Ayo
+Obi
+Manager
+Datti
